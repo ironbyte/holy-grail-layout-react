82fc30044b154d6b8f2d863fd696a8c56e9c8965
Moncy Gopalan <illusivespectre@gmail.com>
2019-07-23 15:24:35 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import './normalize.scss';
+import './index.scss';
+
 import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
